78c240da30b57c42df93f13e2d49feb2755a02f8
msouza-quant <138340846+msouza2023@users.noreply.github.com>
2023-07-06 13:16:46 -0700

    Update setup.py

--- magick-py/setup.py
+++ magick-py/setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="magick",
-    version='0.1',
+    version='0.2',
     packages=find_packages(include=['src', \
                     'src.experiments', \
                     'src.primitives', \
                     'src.utils']),
-    author="steinkirch.eth",
+    author="bt3gl",
     install_requires=['python-dotenv'],
     entry_points={
         'console_scripts': ['magick=src.main:run']
